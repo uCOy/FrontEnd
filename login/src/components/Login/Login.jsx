b5d49b66a512ec3857b385dc81c5ebedfc28874b
RICHARD HENRIQUE COUTO DA ROCHA <richard.hcrocha@senacsp.edu.br>
2022-07-21 22:07:03 -0300

    FrontEnd 21.07.2022

--- login/src/components/Login/Login.jsx
+++ login/src/components/Login/Login.jsx
@@ -11,6 +11,12 @@ export function Login() {
     password: ''
   })
 
+  const [status, setStatus] = useState({
+    type: '',
+    mensagem: '',
+    loading: false
+  })
+
   const valorInput = e => setUser({ 
     ...user, 
     [e.target.name]: e.target.value
@@ -24,45 +30,65 @@ export function Login() {
     const headers = {
       'Content-Type': 'application/json'
     }
+
+    setStatus({
+      loading:true,
+    })
+
     await api.post("login", user, {headers})
     .then((response) => {
-      console.log(response);
+      // console.log(response);
+      setStatus({
+        type: 'success',
+        mensagem: response.data.mensagem
+      })
     }).catch((err) => {
+      setStatus({
+        
+          type: 'error',
+          mensagem: 'Erro: tente mais tarde...'
+      })
       if(err.response){
-        console.log(err.response)
-      } else {
-        console.log("Erro: tente mais tarde...")
+        // console.log(err.response);
+        setStatus({
+          type: 'error',
+          mensagem: err.response.data.mensagem,
+          loading: false
+        })
       }
+      // console.log("Erro: tente mais tarde...");  
     })
   }
-
-
-
     return(
-        <>
-        <Container className="box">
-            <Form onSubmit={loginSubmit} className="borderForm">
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" name="email" onChange={valorInput} placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
-    
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" name="password" onChange={valorInput} placeholder="Password" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </Form>
-        </Container>
-        </>
+        <div className="background">
+          <Container className="box">
+              <Form onSubmit={loginSubmit} className="borderForm">
+                {status.type == 'error' ? <p>{status.mensagem}</p>: ""}
+                {status.type == 'success' ? <p>{status.mensagem}</p>: ""}
+
+                {status.loading ? <p>Validando...</p>: ""}
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" name="email" onChange={valorInput} placeholder="Enter email" />
+                  <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                    {/* Nunca compartilharemos seu e-mail com mais ninguém */}
+                  </Form.Text>
+                </Form.Group>
+      
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" name="password" onChange={valorInput} placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                  <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button id="button" variant="primary" type="submit" >
+                  Submit
+                </Button>
+              </Form>
+          </Container>
+        </div>
     )
 
 }
